8e2f834aca056155ef21280335eae6b4baf2b4dc
imwillim <116113849+imwillim@users.noreply.github.com>
2024-08-04 01:15:25 +0700

    TCH-16 add product serializer (#19)

---
+++ spec/serializers/product_serializer_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ProductSerializer, type: :serializer do
+  let(:tea_category) { build(:category, name: 'tea_category') }
+  let(:milk_tea) { build(:product, name: 'milk_tea', category: tea_category) }
+
+  let(:milk_tea_json) { ProductSerializer.new(milk_tea).serializable_hash }
+
+  context 'when serializing product' do
+    let(:expected_milk_tea_json) do
+      {
+        id: milk_tea.id,
+        name: milk_tea.name,
+        price: milk_tea.price,
+        image_urls: milk_tea.image_urls,
+        description: milk_tea.description,
+        sizes: [],
+        toppings: [],
+        tag: nil
+      }
+    end
+    it 'returns the expected json of product' do
+      expect(milk_tea_json).to eq expected_milk_tea_json
+    end
+  end
+end
