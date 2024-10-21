ad5a612ad74abb8cee88d6f2cec165a15740f487
imwillim <116113849+imwillim@users.noreply.github.com>
2024-10-22 00:31:35 +0700

    TCH-37: remove redundant property of before hook

--- spec/support/redis_mock.rb
+++ spec/support/redis_mock.rb
@@ -3,7 +3,7 @@
 RSpec.shared_context 'redis mock' do
   let(:redis) { MockRedis.new }
 
-  before(:each) do
+  before do
     allow(RedisWrapper).to receive(:redis_pool).and_return(ConnectionPool.new { redis })
   end
 end
