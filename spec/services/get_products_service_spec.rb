003d7fbaa3706e00b9237826c2127dbd521f16ad
imwillim <116113849+imwillim@users.noreply.github.com>
2024-10-22 00:40:19 +0700

    TCH-39: add cache for get products (WIP)

--- spec/services/get_products_service_spec.rb
+++ spec/services/get_products_service_spec.rb
@@ -20,6 +20,10 @@ describe GetProductsService do
     context 'when category_id does not exist in db' do
       let(:category_id) { SecureRandom.uuid }
 
+      before do
+        allow(CacheManager).to receive(:fetch_value).and_return(nil)
+      end
+
       it 'returns error' do
         service.call
 
@@ -29,146 +33,329 @@ describe GetProductsService do
     end
 
     describe '#call' do
-      context 'when a category does not have a parent category' do
-        let(:category_id) { all_category.id }
-        let(:expected_result) do
-          [
-            {
-              name: 'Coffee category',
-              products: match_array([
-                                      {
-                                        id: iced_coffee.id,
-                                        name: iced_coffee.name,
-                                        price: iced_coffee.price,
-                                        thumbnail: iced_coffee.thumbnail,
-                                        tag: {
-                                          color: nil,
-                                          name: nil
-                                        }
-                                      },
-                                      {
-                                        id: americano.id,
-                                        name: americano.name,
-                                        price: americano.price,
-                                        thumbnail: americano.thumbnail,
-                                        tag: {
-                                          color: nil,
-                                          name: nil
-                                        }
-                                      }
-                                    ])
-
-            },
-            {
-              name: 'Milk Tea category',
-              products: match_array([{
-                                      id: milk_tea.id,
-                                      name: milk_tea.name,
-                                      price: milk_tea.price,
-                                      thumbnail: milk_tea.thumbnail,
-                                      tag: {
-                                        color: nil,
-                                        name: nil
-                                      }
-                                    }])
-            },
-            {
-              name: 'Hi Tea category',
-              products: match_array([{
-                                      id: hi_tea.id,
-                                      name: hi_tea.name,
-                                      price: hi_tea.price,
-                                      thumbnail: hi_tea.thumbnail,
-                                      tag: {
-                                        color: nil,
-                                        name: nil
-                                      }
-                                    }])
-            }
-          ]
-        end
-
-        it 'returns categories having products' do
-          service.call
-
-          expect(service.success?).to eq true
-          expect(service.result.size).to eq 3
-
-          expect(service.result).to match_array(expected_result)
-        end
-      end
-
-      context 'when a category has a parent category' do
-        context 'when a category has children categories' do
-          let(:category_id) { tea_category.id }
+      context 'when products within category exists in cache' do
+        context 'when a category does not have a parent category' do
+          let(:category_id) { all_category.id }
           let(:expected_result) do
             [
               {
-                name: 'Hi Tea category',
-                products: match_array([{
-                                        id: hi_tea.id,
-                                        name: hi_tea.name,
-                                        price: hi_tea.price,
-                                        thumbnail: hi_tea.thumbnail,
-                                        tag: {
-                                          color: nil,
-                                          name: nil
+                name: 'Coffee category',
+                products: match_array([
+                                        {
+                                          id: iced_coffee.id,
+                                          name: iced_coffee.name,
+                                          price: iced_coffee.price,
+                                          thumbnail: iced_coffee.thumbnail,
+                                          tag: {
+                                            color: nil,
+                                            name: nil
+                                          }
+                                        },
+                                        {
+                                          id: americano.id,
+                                          name: americano.name,
+                                          price: americano.price,
+                                          thumbnail: americano.thumbnail,
+                                          tag: {
+                                            color: nil,
+                                            name: nil
+                                          }
                                         }
-                                      }])
+                                      ])
+
               },
               {
                 name: 'Milk Tea category',
                 products: match_array([{
-                                        id: milk_tea.id,
-                                        name: milk_tea.name,
-                                        price: milk_tea.price,
-                                        thumbnail: milk_tea.thumbnail,
-                                        tag: {
-                                          color: nil,
-                                          name: nil
-                                        }
-                                      }])
+                                         id: milk_tea.id,
+                                         name: milk_tea.name,
+                                         price: milk_tea.price,
+                                         thumbnail: milk_tea.thumbnail,
+                                         tag: {
+                                           color: nil,
+                                           name: nil
+                                         }
+                                       }])
+              },
+              {
+                name: 'Hi Tea category',
+                products: match_array([{
+                                         id: hi_tea.id,
+                                         name: hi_tea.name,
+                                         price: hi_tea.price,
+                                         thumbnail: hi_tea.thumbnail,
+                                         tag: {
+                                           color: nil,
+                                           name: nil
+                                         }
+                                       }])
               }
             ]
           end
-          it 'returns children categories having products' do
+
+          before do
+            allow(CacheManager).to receive(:fetch_value).and_return(expected_result.to_json)
+          end
+
+          it 'returns categories having products' do
             service.call
 
+
             expect(service.success?).to eq true
-            expect(service.result.size).to eq 2
+            expect(service.result.size).to eq 3
+            expect(service.result.to_json).to eq(expected_result.to_json)
+          end
+        end
+
+        context 'when a category has a parent category' do
+          context 'when a category has children categories' do
+            let(:category_id) { tea_category.id }
+            let(:expected_result) do
+              [
+                {
+                  name: 'Hi Tea category',
+                  products: match_array([{
+                                           id: hi_tea.id,
+                                           name: hi_tea.name,
+                                           price: hi_tea.price,
+                                           thumbnail: hi_tea.thumbnail,
+                                           tag: {
+                                             color: nil,
+                                             name: nil
+                                           }
+                                         }])
+                },
+                {
+                  name: 'Milk Tea category',
+                  products: match_array([{
+                                           id: milk_tea.id,
+                                           name: milk_tea.name,
+                                           price: milk_tea.price,
+                                           thumbnail: milk_tea.thumbnail,
+                                           tag: {
+                                             color: nil,
+                                             name: nil
+                                           }
+                                         }])
+                }
+              ]
+            end
+
+            before do
+              allow(CacheManager).to receive(:fetch_value).and_return(expected_result.to_json)
+            end
+
+            it 'returns children categories having products' do
+              service.call
+
+
+              expect(service.success?).to eq true
+              expect(service.result.size).to eq 2
+              expect(service.result.to_json).to eq(expected_result.to_json)
+            end
+          end
+
+          context 'when a category does not have children categories' do
+            let(:category_id) { milk_tea_category.id }
+            let(:expected_result) do
+              [
+                {
+                  name: 'Milk Tea category',
+                  products: [
+                    {
+                      id: milk_tea.id,
+                      name: 'Milk Tea',
+                      price: 9.99,
+                      thumbnail: 'MyString',
+                      tag: {
+                        name: nil,
+                        color: nil
+                      }
+                    }
+                  ]
+                }
+              ]
+            end
+
+            before do
+              allow(CacheManager).to receive(:fetch_value).and_return(expected_result.to_json)
+            end
+
+            it 'returns that category having products' do
+              service.call
+
+              expect(service.success?).to eq true
+              expect(service.result.size).to eq 1
+              expect(service.result.to_json).to eq(expected_result.to_json)
+            end
+          end
+        end
+      end
+
+      context 'when products within category does not exist in cache' do
+        include_context 'redis mock'
+        context 'when a category does not have a parent category' do
+          let(:category_id) { all_category.id }
+          let(:expected_result) do
+            [
+              {
+                name: 'Coffee category',
+                products: match_array([
+                                        {
+                                          id: iced_coffee.id,
+                                          name: iced_coffee.name,
+                                          price: iced_coffee.price,
+                                          thumbnail: iced_coffee.thumbnail,
+                                          tag: {
+                                            color: nil,
+                                            name: nil
+                                          }
+                                        },
+                                        {
+                                          id: americano.id,
+                                          name: americano.name,
+                                          price: americano.price,
+                                          thumbnail: americano.thumbnail,
+                                          tag: {
+                                            color: nil,
+                                            name: nil
+                                          }
+                                        }
+                                      ])
+
+              },
+              {
+                name: 'Milk Tea category',
+                products: match_array([{
+                                         id: milk_tea.id,
+                                         name: milk_tea.name,
+                                         price: milk_tea.price,
+                                         thumbnail: milk_tea.thumbnail,
+                                         tag: {
+                                           color: nil,
+                                           name: nil
+                                         }
+                                       }])
+              },
+              {
+                name: 'Hi Tea category',
+                products: match_array([{
+                                         id: hi_tea.id,
+                                         name: hi_tea.name,
+                                         price: hi_tea.price,
+                                         thumbnail: hi_tea.thumbnail,
+                                         tag: {
+                                           color: nil,
+                                           name: nil
+                                         }
+                                       }])
+              }
+            ]
+          end
+
+          before do
+            allow(CacheManager).to receive(:fetch_value).and_return(nil)
+            allow(CacheManager).to receive(:assign_value).and_return(category_id, expected_result.to_json)
+          end
+
+          it 'returns categories having products' do
+            service.call
+
+            expect(redis.set(category_id, expected_result.to_json))
+
+            expect(service.success?).to eq true
+            expect(service.result.size).to eq 3
             expect(service.result).to match_array(expected_result)
           end
         end
 
-        context 'when a category does not have children categories' do
-          let(:category_id) { milk_tea_category.id }
-          let(:expected_result) do
-            [
-              {
-                name: 'Milk Tea category',
-                products: [
-                  {
-                    id: milk_tea.id,
-                    name: 'Milk Tea',
-                    price: 9.99,
-                    thumbnail: 'MyString',
-                    tag: {
-                      name: nil,
-                      color: nil
-                    }
-                  }
-                ]
-              }
-            ]
+        context 'when a category has a parent category' do
+          context 'when a category has children categories' do
+            let(:category_id) { tea_category.id }
+            let(:expected_result) do
+              [
+                {
+                  name: 'Hi Tea category',
+                  products: match_array([{
+                                           id: hi_tea.id,
+                                           name: hi_tea.name,
+                                           price: hi_tea.price,
+                                           thumbnail: hi_tea.thumbnail,
+                                           tag: {
+                                             color: nil,
+                                             name: nil
+                                           }
+                                         }])
+                },
+                {
+                  name: 'Milk Tea category',
+                  products: match_array([{
+                                           id: milk_tea.id,
+                                           name: milk_tea.name,
+                                           price: milk_tea.price,
+                                           thumbnail: milk_tea.thumbnail,
+                                           tag: {
+                                             color: nil,
+                                             name: nil
+                                           }
+                                         }])
+                }
+              ]
+            end
+
+            before do
+              allow(CacheManager).to receive(:fetch_value).and_return(nil)
+              allow(CacheManager).to receive(:assign_value).and_return(category_id, expected_result.to_json)
+            end
+
+            it 'returns children categories having products' do
+              service.call
+
+              expect(redis.set(category_id, expected_result.to_json))
+
+              expect(service.success?).to eq true
+              expect(service.result.size).to eq 2
+              expect(service.result).to match_array(expected_result)
+            end
           end
 
-          it 'returns that category having products' do
-            service.call
+          context 'when a category does not have children categories' do
+            let(:category_id) { milk_tea_category.id }
+            let(:expected_result) do
+              [
+                {
+                  name: 'Milk Tea category',
+                  products: [
+                    {
+                      id: milk_tea.id,
+                      name: 'Milk Tea',
+                      price: '9.99',
+                      thumbnail: 'MyString',
+                      tag: {
+                        name: nil,
+                        color: nil
+                      }
+                    }
+                  ]
+                }
+              ]
+            end
 
-            expect(service.success?).to eq true
-            expect(service.result.size).to eq 1
-            expect(service.result.first[:products].size).to eq 1
-            expect(service.result.first[:products].first[:id]).to eq expected_result.first[:products].first[:id]
+            before do
+              allow(CacheManager).to receive(:fetch_value).and_return(nil)
+              allow(CacheManager).to receive(:assign_value).with(category_id, expected_result.to_json)
+            end
+
+            it 'returns that category having products' do
+              service.call
+
+              expect(redis.set(category_id, expected_result.to_json))
+
+              expect(service.success?).to eq true
+              expect(service.result.size).to eq 1
+              expect(service.result.first[:products].size).to eq 1
+              expect(service.result.first[:products].first[:id]).to eq expected_result.first[:products].first[:id]
+            end
           end
         end
       end
