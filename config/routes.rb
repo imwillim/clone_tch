b43a596734be0333b9ba50f1e56d00a6c2cfe5de
imwillim <116113849+imwillim@users.noreply.github.com>
2024-11-20 23:28:04 +0700

    TCH-46 - fix for GET endpoint only

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@
 Rails.application.routes.draw do
   scope '/api/v1/tch' do
     resources :products, only: %i[show index]
-    resources :stores do
+    resources :stores, only: %i[show] do
       get 'directions', on: :member
-      get 'show'
     end
   end
 end
