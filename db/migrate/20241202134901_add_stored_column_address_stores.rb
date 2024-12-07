87bf19c59066479f1988b1c9a36d06128977a5a0
imwillim <116113849+imwillim@users.noreply.github.com>
2024-12-07 21:47:39 +0700

    TCH-47 - add virtual column

--- db/migrate/20241202134901_add_stored_column_address_stores.rb
+++ db/migrate/20241202134901_add_stored_column_address_stores.rb
@@ -3,7 +3,7 @@
 class AddStoredColumnAddressStores < ActiveRecord::Migration[7.1]
   def change
     add_column :addresses, :computed_address, :virtual, type: :string,
-               as: "house_number || ', ' || street || ', ' || ward || ', ' || district",
+               as: "house_number || ' ' || street || ', ' || ward || ', ' || district",
                stored: true
   end
 end
