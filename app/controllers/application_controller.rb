111b4651a70cbd14643ccab9d976250f1ebca282
imwillim <116113849+imwillim@users.noreply.github.com>
2025-03-12 23:10:41 +0700

    Add authenticate token method, current_user attribute and errors for auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,39 @@
 class ApplicationController < ActionController::API
   include ErrorHandling
 
+  attr_reader :current_user
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record
+
+  rescue_from Redis::BaseError, with: :render_redis_error
+  rescue_from JWT::EncodeError, with: :render_encode_jwt_error
+  rescue_from JWT::DecodeError, with: :render_decode_jwt_error
+
   rescue_from ::CustomErrors::InvalidParams, with: :render_invalid_params
   rescue_from ::CustomErrors::UnprocessableService, with: :render_unprocessable_service
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
-  rescue_from Redis::BaseError, with: :render_redis_error
-  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record
+  rescue_from ::CustomErrors::UnauthorizedError, with: :render_unauthorized
 
   before_action do
     raise ::CustomErrors::InvalidParams, safe_params.errors(full: true).messages.join(', ') if safe_params&.failure?
+  end
+
+  def authenticate_token
+    token = request.headers['Authorization']
+
+    raise ::CustomErrors::UnauthorizedError, 'Unauthorized' if token.blank?
+    raise ::CustomErrors::UnauthorizedError, 'Token invalid' if CacheManager.exists?("black_list #{token}")
+
+    payload = JsonWebTokenManager.decode(token)
+
+    raise ::CustomErrors::UnauthorizedError, 'Token timeout' if payload['exp'] < Time.now.to_i
+
+    current_user_with_payload(payload)
+  end
+
+  private
+
+  def current_user_with_payload(payload)
+    @current_user = User.find(payload['user_id'])
   end
 end
