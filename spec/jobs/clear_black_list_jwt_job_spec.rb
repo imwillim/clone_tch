683eee72700bdb39b4e877c1610c6dd33abe4d54
imwillim <116113849+imwillim@users.noreply.github.com>
2025-03-15 16:36:36 +0700

    Remove jobs for unnecessary requirement

--- spec/jobs/clear_black_list_jwt_job_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-# TODO: write rspec
-RSpec.describe ClearBlackListJwtJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
