e3765cf6f095bb4886e86ea0d8e5eb9489d349d6
imwillim <116113849+imwillim@users.noreply.github.com>
2025-03-17 22:18:48 +0700

    Disable rubocop

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,10 @@ class Product < ApplicationRecord
 
   has_many :sizes, dependent: :destroy
   has_many :toppings, dependent: :destroy
+
+  # rubocop:disable Rails/HasAndBelongsToMany
   has_and_belongs_to_many :tags
+  # rubocop:enable Rails/HasAndBelongsToMany
 
   belongs_to :category
 
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,5 +4,7 @@ class Tag < ApplicationRecord
   validates :name, presence: true
   validates :color, presence: true
 
+  # rubocop:disable Rails/HasAndBelongsToMany
   has_and_belongs_to_many :products
+  # rubocop:enable Rails/HasAndBelongsToMany
 end
